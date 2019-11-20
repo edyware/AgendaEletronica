59ebd2027dc0052cfbe8d3c2f263115f4234696e
Edevaldo Valente <valente.ufpa@gmail.com>
2019-11-20 10:37:57 -0300

    Update do video 3

--- Agenda Eletronica/js/main.js
+++ Agenda Eletronica/js/main.js
@@ -37,5 +37,51 @@ function createRecordatorio(){
 		mostrarError();
 		return;			
 	}
-	limparError(); 
+
+	limparError();
+
+	//Criar as variaveis para tempo
+	var referencia = new Date();
+	var getElementById = referencia.getTime();
+	var data = referencia.toLocalDateString();
+	var text = conteudoTextArea;
+
+	//JSON = notação de objeto JS
+	var recordatorio = {"id" : id,"data" : data ,"texto" : texto};
+
+	//Function para comprovar se existe lembrete ou recordatorio
+	comprovarRecordatorio(recordatorio);
+}
+	//Função para comprovar se existe lembrete
+	function comprovarRecordatorio(recordatorio){
+		var recordatorioExistentes = localStorage.getItem("recordatorio");
+		if(recordatorioExistentes == null || recordatorioExistentes == ""){
+			var recordatorios = [];
+			recordatorios.push(recordatorio);
+
+			//Save 
+			saveRecordatorios(recordatorios);
+		}else{
+			var recordatoriosRecuperados = JSON.parse(recordatorioExistentes);
+			recordatoriosRecuperados.push(recordatorio);
+			saveRecordatorios(recordatorios);
+		}
+	}
+	
+}
+
+//Função para salvar lembrete ou recordatorio
+function saveRecordatorios(recordatorios){
+	var recordatoriosJSON = JSON.stingify(recordatorios);
+	localStorage.setItem("re]", recordatoriosJSON);
+
+}
+
+função mostrarRecordatorios(){
+	var html = "";
+	var recordatorioExistentes = localStorage.getItem("recordatorios");
+	 if(recordatorioExistentes == null || recordatorioExistentes == ""){
+	 	html = "Não existe nenhum lembrete...";
+	 	document.getElementById("recordatorios").innerHTML = ;
+	 }
 }
